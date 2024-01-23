5f05e74199739b05ee8cf7d7445face2cfcf76e9
Yevand <99153027+Yevand@users.noreply.github.com>
2024-01-23 19:35:37 +0100

    jsBasics directory and task1-4.3 files added

---
+++ jsBasics/task4.1.js
@@ -0,0 +1,7 @@
+// task 4.1
+const pi = Math.PI
+
+const radius = 17
+
+let circleArea = pi * radius**2
+console.log("Area of circle:", circleArea)
---
+++ jsBasics/task4.3.js
@@ -0,0 +1,7 @@
+// task 4.3
+const radius = 3.4
+
+const height = 9.7
+
+let cylinderVolume = radius * height
+console.log("Volume of cylinder:", Number(cylinderVolume.toFixed(2)))
