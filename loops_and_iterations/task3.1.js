bdce5314cae44cfc7f7b591e7c0701a36c36ac63
Yevand <99153027+Yevand@users.noreply.github.com>
2024-01-30 22:12:12 +0100

    changes have beed added to task3.1.js and task3.2.js

--- loops_and_iterations/task3.1.js
+++ loops_and_iterations/task3.1.js
@@ -1,4 +1,4 @@
-let number = 3
+const number = 3
 
 for (let i =1; i <= 10; i++) {
     console.log(`${number} x ${i} = ${number*i}`)
--- loops_and_iterations/task3.2.js
+++ loops_and_iterations/task3.2.js
@@ -1,5 +1,5 @@
-let i = 1
-let number = 3
+const i = 1
+const number = 3
 
 while (i <= 10) {
     console.log(`${number} x ${i} = ${number*i}`)
