66835d3d633bb297a2b7beaa4d9ea8110cf75aed
Yevand <99153027+Yevand@users.noreply.github.com>
2024-02-03 22:17:33 +0100

    task1.js and task2.js fixed

--- functions_intro/task2.js
+++ functions_intro/task2.js
@@ -1,9 +1,5 @@
 function isAdult(age) {
-    if(age >= 18) {
-        return true
-    } else {
-        return false
-    }
+    return age >= 18
 }
 
 console.log("Чи є особа дорослою? -", isAdult(25))
