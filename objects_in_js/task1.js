c38d4fb157197bf3e26803f9d0578da00211666f
Yevand <99153027+Yevand@users.noreply.github.com>
2024-02-11 21:40:09 +0100

    objects_in_js directory and task1.js - task5.js files have been added

---
+++ objects_in_js/task1.js
@@ -0,0 +1,8 @@
+const student = {
+    firstName: "Тарас",
+    lastName: "Шевченко",
+    age: 47,
+    courses: "Література"
+}
+
+console.log(student)
---
+++ objects_in_js/task4.js
@@ -0,0 +1,11 @@
+const person = {
+    firstName: "Іван",
+    lastName: "Іванов",
+    age: 27
+}
+
+person.email = "test@gmail.com"
+
+delete person.age
+
+console.log(person)
